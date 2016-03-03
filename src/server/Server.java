6f59d8cd0cd0a23c9f07f23cc1273e5a0b8c425e
JRWhiffing <jrwhiffing@gmail.com>
2016-03-03 11:00:51 +0000

    server tings

--- src/server/Server.java
+++ src/server/Server.java
@@ -12,7 +12,7 @@ public class Server {
 	private static boolean active; 
 	private static final HashMap<String,Room> ROOMS = new HashMap<String, Room>(); //Have max limit?
 	//to be used to view all rooms without knowing keys by storing key with room number, i.e. view active rooms.
-	private static final HashMap<Integer, String> roomKeys = new HashMap<Integer, String>();
+	private static final HashMap<Integer, String> ROOMKEYS = new HashMap<Integer, String>();
 	private static int lastRoom = 0;
 	private static ServerListener serverListener;
 	
@@ -66,13 +66,33 @@ public class Server {
 		return key;
 	}
 	
+	public static synchronized void closeRoom(String key){
+		//Server.ROOMS.get(key).close();
+		//while(Server.ROOMS.get(key).getState() != "Finished"){ }
+		Server.ROOMS.remove(key);
+		for(int i = 0; i < lastRoom; i++){
+			if(Server.ROOMKEYS.containsKey(i)){
+				if(Server.ROOMKEYS.get(i).equals(key)){
+					Server.ROOMKEYS.remove(i);
+					break;
+				}
+			}
+		}
+	}
+	
 	public static synchronized void createRoom(int clientID, String roomName, String clientName, double[] MACAddress){
 		String key = generateRoomKey();
-		//ROOMS.put(key, new Room(roomName, clientID, clientName, MAC Address));
+		//Server.ROOMS.put(key, new Room(roomName, clientID, clientName, MAC Address));
+		Server.ROOMKEYS.put(lastRoom, key);
+		lastRoom++;
 		RoomKeyPacket rkp = new RoomKeyPacket();
 		rkp.putRoomKey(key);
 		Server.sendPacket(clientID, rkp);
 		serverListener.setRoomKey(clientID, key);
+	}
+	
+	public static void printRooms(){
+		
 	}
 	
 }
