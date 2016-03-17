6fecacdd9cec2ad8949461c19e04cea33cedb974
Liefdrag <adamhobbs123@gmail.com>
2016-03-17 18:58:15 +0100

    Commenting and Organising

--- src/room/Room.java
+++ src/room/Room.java
@@ -11,34 +11,97 @@ import packets.serverPackets.*;
 import packets.serverPackets.broadcastPackets.*;
 import server.Server;
 
+/**
+ * Class for room functionality
+ * Each game will have its own Room instance, this is used to look after the game instance and all the player instances
+ * @author James and Adam
+ *
+ */
 public class Room {
 
-	private int roomNumber;
+	private int roomNumber; //Is this needed?
 	private String roomName;
 	private Game currentGame;
+	private int roomSize;
+	
 	private enum State {
 		GAME, LOBBY, STARTING, ENDING, PAUSED, FINISHED
 	}
 	private State roomState;
-	private int hostID;
+
 	private Leaderboard leaderboard;
-	private List<Player> players = Collections.synchronizedList( new ArrayList<Player>());
-	private HashMap<Integer, Integer> playerIDMap = new HashMap<Integer, Integer>(); //Player ClientID -> Player Instance in players
-	private int maxPlayerID = 0;
-	private int roomSize;
+	private List<Player> players;
+	private HashMap<Integer, Integer> playerIDMap; //Player ClientID -> Player Instance in players
+	private int maxPlayerID;
+	private int hostID;
 	
+	/**
+	 * Constructor that sets up all the global variables and creates a new game instance and a new leaderboard
+	 * @param roomName - The name of the Room as a String
+	 * @param clientID - The integer ID of the Player that created the room (This will be the host)
+	 * @param hostName - The name of the player as a String
+	 * @param MACAddress - The MAC Address of the player
+	 */
 	public Room(String roomName, int clientID, String hostName, double[] MACAddress) {
 		this.roomName = roomName;
 		this.hostID = clientID;
-		currentGame = new Game((byte)0x00, 0, 0, 0, new double[] {0.0,0.0}, 0, 0);
-		addPlayer(hostName, MACAddress, clientID);
-		leaderboard = new Leaderboard();
+		//Sets up global variables
+		maxPlayerID = 0;
+		players = Collections.synchronizedList( new ArrayList<Player>());
+		playerIDMap = new HashMap<Integer, Integer>();
+		currentGame = new Game((byte)0x00, 0, 0, 0, new double[] {0.0,0.0}, 0, 0); //Creates a new game
+		addPlayer(hostName, MACAddress, clientID); //Adds the first player to the player list
+		roomSize++;
+		leaderboard = new Leaderboard(); //Creates a new leaderboard
 		leaderboard.addPlayer(clientID, hostName);	
 		roomState = State.LOBBY;
 	}
 	
-	public void changeGameType(byte gameType) {
-		currentGame.changeGameType(gameType);
+	private void startGame(){
+		//Needs to check what the vote result is countVotes() - Will return the game mode voted for
+	}
+	
+	public void endGame() {
+		//Functionality to end the game
+	}
+	
+	public void catchPerformed(int clientID) {
+		int targetID = players.get(playerIDMap.get(clientID)).getTarget(); // Target of player performing catch
+		if (checkCaptured(targetID)) {
+			players.get(playerIDMap.get(targetID)).beenCaught(); //Changes the state of the player to changing
+			leaderboard.updatePlayerScore(clientID, 100);
+			//SEND LEADERBOARD PACKETS TO ALL CLIENTS
+			//Change the Target
+		}
+		else {
+			//Something needs to happen if response didn't come in
+		}
+	}
+	
+	private boolean checkCaptured(int targetID) {
+		long currentTime = System.currentTimeMillis();
+		long maxTime = currentTime + 10000; //Checks for 10 seconds
+		while(System.currentTimeMillis() < maxTime) {
+			if (players.get(playerIDMap.get(targetID)).checkCaught()) {
+				return true;
+			}
+		}
+		return false;
+		//Needs to see if captured has been called by the targetID
+	}
+	
+	
+	///Player presses caught button but pursuer hasn't pressed button yet. - This may need changing if the caught button only appears when pursuer presses button
+	public void captured(int clientID) {
+		players.get(playerIDMap.get(clientID)).captured();
+		long currentTime = System.currentTimeMillis();
+		long maxTime = currentTime + 10000; //Checks for 10 seconds
+		while(System.currentTimeMillis() < maxTime) {
+			if (players.get(playerIDMap.get(clientID)).checkContinue()) {
+				//Change the pursuer
+			}
+		}
+		//Something needs to happen if player has not been caught
 	}
 	
 	private void assignTargets(int clientID){
@@ -122,81 +185,153 @@ public class Room {
 		}
 	}
 	
-	private void broadcast(Packet broadcastPacket){
-		for(int i = 0; i < players.size(); i++){
-			if(!players.get(i).getState().equals("DISCONNECTED") && !players.get(i).getState().equals("KICKED")){
-				Server.sendPacket(players.get(i).getID(), broadcastPacket);
-			}
-		}
-	}
-	
-	private void startGame(){
-		//Needs to check what the vote result is countVotes() - Will return the game mode voted for
-	}
-	
+	/**
+	 * Method that adds a new player to the game
+	 * @param playerName - The name of the new player
+	 * @param MACAddress - The MAC Address of the new player
+	 * @param clientID - The integer ID of the new player
+	 */
 	public void addPlayer(String playerName, double[] MACAddress, int clientID) {
 		//need to broadcast relevant leaderboard and player joined
-		if(roomSize == 16){
+		//Checks if the room is full
+		if(roomSize == 16) {
 			NAKPacket np = new NAKPacket();
 			np.setNAK(Packet.NAK_ROOM_FULL);
 			Server.sendPacket(clientID, np);
 			return;
 		}
+		//Checks if the players data has been previously saved
 		for(int i = 0; i < players.size(); i++){
-			if(players.get(i).getState().equals("DISCONNECTED") && players.get(i).getMACAddress().equals(MACAddress)){
+			if(players.get(i).getState().equals("DISCONNECTED") && players.get(i).getMACAddress().equals(MACAddress)) {
 				players.get(i).setID(clientID);
 				playerIDMap.put(clientID, i);
 				players.get(i).rejoined();
 				return;
 			}
 		}
-		players.add(new Player(playerName, MACAddress));
+		players.add(new Player(playerName, MACAddress)); //Creates a new Player instance and adds it to list of players
 		playerIDMap.put(clientID, players.size());
 		leaderboard.addPlayer(clientID, playerName);
-		if(clientID > maxPlayerID){ maxPlayerID = clientID; }
+		//Increases the Max Player ID if its a new player
+		if(clientID > maxPlayerID) { 
+			maxPlayerID = clientID; 
+		}
 		roomSize++;
 	}
 	
 	//Currently this does kicking + Removing - May want to change this at a later point
+	/**
+	 * Method that removes the player from the game
+	 * @param clientID - The integer ID of the player being removed
+	 */
 	public void quitPlayer(int clientID) {
 		roomSize--;
 		players.get(playerIDMap.get(clientID)).removePlayer();
 		leaderboard.removePlayer(clientID);
+		//Needs to set a new host if the player is the host
 		if (clientID == hostID) {
 			setNewHost();			
 		}
 	}
 	
+	/**
+	 * Method that changes the host's ID
+	 * @param hostID - The integer ID of the new host
+	 */
 	public void changeHost(int hostID) {
-		//Changes the host to the new ID
 		this.hostID = hostID;
 	}
 	
-	public void setNewHost() {
-		ArrayList<Integer> playersAvailable = new ArrayList<Integer>();
+	/**
+	 * Method that is used to set up a new host
+	 */
+	private void setNewHost() {
+		ArrayList<Integer> playersAvailable = new ArrayList<Integer>(); //List of all the players that could be the new host
 		for(int i = 0; i < players.size(); i++){
-			if(!players.get(i).getState().equals("DISCONNECTED") && !players.get(i).getState().equals("KICKED")){
+			//Player has to have an active state
+			if(!players.get(i).getState().equals("DISCONNECTED") && !players.get(i).getState().equals("KICKED")) {
 				playersAvailable.add(players.get(i).getID());
 			}
 		}
-		Random rng = new Random();
-		int numberOfPlayersInGame = playersAvailable.size(); //Number of players in game
-		int newHost = rng.nextInt(numberOfPlayersInGame);
+		Random numberGenerator = new Random();
+		int players = playersAvailable.size(); //Number of players in game
+		int newHost = numberGenerator.nextInt(players);
 		hostID = playersAvailable.get(newHost); //Use this integer number in the arraylist to get the player in that position
 		Server.sendPacket(hostID, new HostPacket()); //Alert client that they are the new host
-		NewHostPacket nhp = new NewHostPacket();
-		nhp.putPlayerID(hostID);
-		broadcast(nhp); //Tell all clients there is a new host
+		NewHostPacket hostPacket = new NewHostPacket();
+		hostPacket.putPlayerID(hostID);
+		broadcast(hostPacket); //Tells all clients that there is a new host
 	}
 	
+	/**
+	 * Method that sends a packet to all active players in the game
+	 * @param broadcastPacket - The packet that needs sending
+	 */
+	private void broadcast(Packet broadcastPacket){
+		for(int i = 0; i < players.size(); i++){
+			//Finds only active players
+			if(!players.get(i).getState().equals("DISCONNECTED") && !players.get(i).getState().equals("KICKED")){
+				Server.sendPacket(players.get(i).getID(), broadcastPacket);
+			}
+		}
+	}
+	
+	/**
+	 * Method to update the location of the player in the player instance
+	 * @param location - Longtitude and Latitude of Player in an array
+	 * @param clientID - The integer ID of the player
+	 */
 	public void setPlayerLocation(double[] location, int clientID) {
 		players.get(playerIDMap.get(clientID)).setPlayerLocation(location);
 	}
 	
+	/**
+	 * Method to set the current ping of the player
+	 * @param ping - The ping of the player as an integer
+	 * @param clientID - The integer ID of the player
+	 */
 	public void setPlayerPing(int ping, int clientID) {
 		players.get(playerIDMap.get(clientID)).setPlayerPing(ping);
 	}
 	
+	/**
+	 * Method that sets the game's time limit
+	 * @param time - The time limit
+	 */
+	public void setTimeLimit(int time) {
+		currentGame.setTimeLimit(time);
+	}
+	
+	/**
+	 * Method that sets the game's score limit
+	 * @param score - The score limit
+	 */
+	public void setScoreLimit(int score) {
+		currentGame.setScoreLimit(score);
+	}
+	
+	/**
+	 * Method that sets the game's boundaries
+	 * @param boundaries - The coordinates of the boundary centre
+	 * @param radius - The radius of the boundary circle
+	 */
+	public void setBoundaryLimit(double[] boundariesCoordinates, int radius) {
+		currentGame.setBoundariesCentre(boundariesCoordinates);
+		currentGame.setBoundariesRadius(radius);
+	}
+	
+	/**
+	 * Method that sets the game's boundary interval period and the percentage the boundary decreases by
+	 * @param updates - Array where first element is interval period and second element is percentage
+	 */
+	public void setBoundariesUpdates(int[] updates) {
+		currentGame.setBoundariesUpdateInterval(updates[0]);
+		currentGame.setBoundariesUpdatePercentage(updates[1]);
+	}
+	
+	/**
+	 * Method that allows voting to take place in a game
+	 */
 	public void toggleVoting() {
 		currentGame.allowVoting();
 	}
@@ -251,6 +386,14 @@ public class Room {
 	}
 	
 	/**
+	 * Method that changes the game mode type manually
+	 * @param gameType - The game type as a byte
+	 */
+	public void changeGameType(byte gameType) {
+		currentGame.changeGameType(gameType);
+	}
+	
+	/**
 	 * Method used to report a player. 
 	 * If more than a third of players report one player then they get kicked
 	 * @param reportedID - The integer ID of the reported player
@@ -284,67 +427,6 @@ public class Room {
 		}
 	}
 	
-	public void catchPerformed(int clientID) {
-		int targetID = players.get(playerIDMap.get(clientID)).getTarget(); // Target of player performing catch
-		if (checkCaptured(targetID)) {
-			players.get(playerIDMap.get(targetID)).beenCaught(); //Changes the state of the player to changing
-			leaderboard.updatePlayerScore(clientID, 100);
-			//SEND LEADERBOARD PACKETS TO ALL CLIENTS
-			//Change the Target
-		}
-		else {
-			//Something needs to happen if response didn't come in
-		}
-	}
-	
-	public boolean checkCaptured(int targetID) {
-		long currentTime = System.currentTimeMillis();
-		long maxTime = currentTime + 10000; //Checks for 10 seconds
-		while(System.currentTimeMillis() < maxTime) {
-			if (players.get(playerIDMap.get(targetID)).checkCaught()) {
-				return true;
-			}
-		}
-		return false;
-		//Needs to see if captured has been called by the targetID
-	}
-	
-	
-	///Player presses caught button but pursuer hasn't pressed button yet. - This may need changing if the caught button only appears when pursuer presses button
-	public void captured(int clientID) {
-		players.get(playerIDMap.get(clientID)).captured();
-		long currentTime = System.currentTimeMillis();
-		long maxTime = currentTime + 10000; //Checks for 10 seconds
-		while(System.currentTimeMillis() < maxTime) {
-			if (players.get(playerIDMap.get(clientID)).checkContinue()) {
-				//Change the pursuer
-			}
-		}
-		//Something needs to happen if player has not been caught
-	}
-	
-	public void setTimeLimit(int time) {
-		currentGame.setTimeLimit(time);
-	}
-	
-	public void setBoundaryLimit(double[] boundaries, int radius) {
-		currentGame.setBoundariesCentre(boundaries);
-		currentGame.setBoundariesRadius(radius);
-	}
-	
-	public void setBoundariesUpdates(int[] updates) {
-		currentGame.setBoundariesUpdateInterval(updates[0]);
-		currentGame.setBoundariesUpdatePercentage(updates[1]);
-	}
-	
-	public void setScoreLimit(int score) {
-		currentGame.setScoreLimit(score);
-	}
-	
-	public void endGame() {
-		//Functionality to end the game
-	}
-	
 	/**
 	 * Method that allows a player to use an ability
 	 * Case statement is used to distinguish between abilities and their functionality
@@ -373,18 +455,34 @@ public class Room {
 		}
 	}
 	
+	/**
+	 * Method that returns the Room Number
+	 * @return - The room number as an integer
+	 */
 	public int getRoomNumber(){
 		return roomNumber;
 	}
-
+	
+	/**
+	 * Method that returns the Room Name
+	 * @return - The room name as a String
+	 */
 	public String getRoomName() {
 		return roomName;
 	}
 
+	/**
+	 * Method that sets the Room Name
+	 * @param roomName - The Room Name as a String
+	 */
 	public void setRoomName(String roomName) {
 		this.roomName = roomName;
 	}
 	
+	/**
+	 * Method that returns the number of players active in the game
+	 * @return - The number of players active in the room
+	 */
 	public int size(){
 		return roomSize;
 	}
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,6 @@
 package server;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
